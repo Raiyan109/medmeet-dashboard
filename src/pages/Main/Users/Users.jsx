4ede5349309d6b0f1f2d91e009afc7a476daa23a
Raiyan109 <kraiyan109@gmail.com>
2025-02-09 16:01:01 +0600

    users page donw

--- src/pages/Main/Users/Users.jsx
+++ src/pages/Main/Users/Users.jsx
@@ -5,6 +5,7 @@ import DashboardModal from "../../../Components/DashboardModal";
 import { IoSearch } from "react-icons/io5";
 import { Link } from "react-router-dom";
 import exlamIcon from "../../../assets/images/exclamation-circle.png";
+import { MdOutlineFileDownload } from "react-icons/md";
 
 const Users = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -16,51 +17,50 @@ const Users = () => {
   };
 
   const columns = [
+    // {
+    //   title: "#SL",
+    //   dataIndex: "transIs",
+    //   key: "transIs",
+    //   render: (text) => <a>{text}</a>,
+    // },
     {
-          title: "#SL",
-          dataIndex: "transIs",
-          key: "transIs",
-          render: (text) => <a>{text}</a>,
-        },
-        {
-          title: "Name",
-          dataIndex: "name",
-          key: "name",
-        },
-        {
-          title: "Email",
-          dataIndex: "Email",
-          key: "Email",
-        },
-        {
-          title: "Phone Number",
-          key: "Phone",
-          dataIndex: "Phone",
-        },
-        {
-          title: "Action",
-          key: "Review",
-          aligen: 'center',
-          render: (_, data) => (
-            <div className="  items-center justify-around textcenter flex " >
-              {/* Review Icon */}
-              <img src={exlamIcon} alt="" className="btn  px-3 py-1 text-sm rounded-full cursor-pointer" onClick={() => showModal(data)} />
-              {/* <Link to={'/reviews'} className="btn bg-black text-white px-3 py-1 text-sm rounded-full">
+      title: "User Name",
+      dataIndex: "username",
+      key: "username",
+    },
+    {
+      title: "User Email",
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      title: "Date",
+      key: "date",
+      dataIndex: "date",
+    },
+    {
+      title: "Action",
+      key: "Review",
+      aligen: 'center',
+      render: (_, data) => (
+        <div className="  items-center justify-around textcenter flex " >
+          {/* Review Icon */}
+          <img src={exlamIcon} alt="" className="btn  px-3 py-1 text-sm rounded-full cursor-pointer" onClick={() => showModal(data)} />
+          {/* <Link to={'/reviews'} className="btn bg-black text-white px-3 py-1 text-sm rounded-full">
                
                 View
               </Link> */}
-            </div>
-          ),
-        },
+        </div>
+      ),
+    },
   ];
 
   const data = [];
   for (let index = 0; index < 20; index++) {
     data.push({
       transIs: `${index + 1}`,
-      name: "Henry",
-      Email: "sharif@gmail.com",
-      Phone: "+12746478994",
+      username: "Henry",
+      email: "sharif@gmail.com",
       Review: "See Review",
       date: "16 Apr 2024",
       _id: index,
@@ -68,8 +68,16 @@ const Users = () => {
   }
 
   return (
-    <div className="rounded-lg border py-4 border-black mt-8 recent-users-table">
-      <h3 className="text-2xl text-black mb-4 pl-2">Recent Users</h3>
+    <div className="rounded-lg bg-[#DDE3E6] mt-8 recent-users-table py-[20px]">
+      <div className="flex justify-between px-2">
+        <h3 className="text-[20px] font-poppins text-[#333333] pl-[20px]">User List</h3>
+        <div className="flex items-center gap-4 mb-6">
+          <DatePicker placeholder="Date" className="w-[164px] h-[36px] rounded-[86px] border-none outline-none" />
+          <Input placeholder="User Name" className="w-[187px] h-[36px] rounded-[86px] border-none outline-none" />
+
+          <button style={{ border: 'none', backgroundColor: '#545454', color: 'white', borderRadius: '50%', padding: '7px' }}><IoSearch size={20} /></button>
+        </div>
+      </div>
       {/* Ant Design Table */}
       <Table
         columns={columns}
@@ -82,44 +90,57 @@ const Users = () => {
       <DashboardModal
         isModalOpen={isModalOpen}
         setIsModalOpen={setIsModalOpen}
-        maxWidth="500px"
+        maxWidth="400px"
+      // backgroundColor={'#E8EBF0'}
       >
-        <div>
-          <h2 className="text-lg text-center mb-4">User Details</h2>
-          <div className="flex justify-between mb-2 text-gray-600">
-            <p>#SL</p>
-            <p>{modalData.transIs}</p>
+        <div className="py-[24px] font-roboto">
+          <h2 className="text-[18px] text-center mb-4 font-roboto">User Details</h2>
+          <div className="border-b border-[#B8C1CF] w-full"></div>
+          <div className="flex justify-between mb-2 text-gray-600 px-[16px] py-[20px]">
+            <p className="text-[14px] ">User Name: </p>
+            <p>{modalData.username}</p>
           </div>
-          <div className="flex justify-between mb-2 text-gray-600">
-            <p>User Name</p>
-            <p>{modalData.name}</p>
-          </div>
-          <div className="flex justify-between mb-2 text-gray-600">
+          <div className="border-b border-[#B8C1CF] w-full"></div>
+          <div className="flex justify-between mb-2 text-gray-600  px-[16px] py-[20px]">
             <p>Email</p>
-            <p>{modalData.Email}</p>
+            <p>{modalData.email}</p>
           </div>
-          <div className="flex justify-between mb-2 text-gray-600">
-            <p>Mobile Phone</p>
+          <div className="border-b border-[#B8C1CF] w-full"></div>
+          <div className="flex justify-between mb-2 text-gray-600  px-[16px] py-[20px]">
+            <p>User name:</p>
+            <p>{modalData.username}</p>
+          </div>
+          <div className="border-b border-[#B8C1CF] w-full"></div>
+          <div className="flex justify-between mb-2 text-gray-600  px-[16px] py-[20px]">
+            <p>A/C number:</p>
             <p>{modalData.Phone}</p>
           </div>
-          <div className="flex justify-between mb-2 text-gray-600">
-            <p>Service</p>
+          <div className="border-b border-[#B8C1CF] w-full"></div>
+          <div className="flex justify-between mb-2 text-gray-600  px-[16px] py-[20px]">
+            <p>A/C holder name</p>
             <p>{modalData.transIs}</p>
           </div>
-          <div className="flex justify-between mb-2 text-gray-600">
-            <p>Date</p>
+          <div className="border-b border-[#B8C1CF] w-full"></div>
+          <div className="flex justify-between mb-2 text-gray-600  px-[16px] py-[20px]">
+            <p>Transaction amount</p>
             <p>{modalData.transIs}</p>
           </div>
-          <div className="flex justify-between mb-2 text-gray-600">
-            <p>Time</p>
+          <div className="border-b border-[#B8C1CF] w-full"></div>
+          <div className="flex justify-between mb-2 text-gray-600  px-[16px] py-[20px]">
+            <p>Doctor name</p>
             <p>{modalData.transIs}</p>
           </div>
-          <div className="flex justify-between mb-2 text-gray-600">
-            <p>Amount</p>
-            <p>{modalData.transIs}</p>
-          </div>
-         
-          
+
+          {/* Buttons */}
+          {/* <div className="flex items-center justify-center gap-[12px] mx-2">
+            <div className="border border-[#545454] w-[200px] h-[48px] rounded-[29px] flex items-center justify-center gap-[10px]">
+              <MdOutlineFileDownload size={20} />
+              <h2 className="font-roboto text-[18px] text-[#333333]">Download</h2>
+            </div>
+            <div className="bg-[#90A4AE] w-[200px] h-[48px] rounded-[29px] flex items-center justify-center gap-[10px] font-roboto text-white text-[18px]">
+              Print
+            </div>
+          </div> */}
         </div>
       </DashboardModal>
     </div>
