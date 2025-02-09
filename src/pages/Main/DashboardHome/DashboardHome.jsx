59589c1d262ad61b0adac4718874669f8cae5015
Raiyan109 <kraiyan109@gmail.com>
2025-02-09 14:34:49 +0600

    total earnings cards

--- src/pages/Main/DashboardHome/DashboardHome.jsx
+++ src/pages/Main/DashboardHome/DashboardHome.jsx
@@ -3,39 +3,40 @@ import BarChartComponent from "./BarChart";
 
 
 const DashboardHome = () => {
- 
+
   return (
     <div className="space-y-[24px]">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-10 lg:gap-x-32  gap-y-10 ">
-        <div className=" flex items-center justify-center gap-6 border border-black px-[24px] py-[20px] rounded-lg space-y-3 bg-white w-96 md:w-full">
-          
-          <div className="text-center">
-            <h3 className="text-[20px]">{"Total Earnings"}</h3>
-            <h3 className="text-[30px] font-extralight">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-10 lg:gap-x-[24px]  gap-y-10 ">
+        {/* Total Earnings */}
+        <div className=" flex items-center  gap-6  px-[24px] py-[20px] rounded-[16px] space-y-3 bg-[#DDE3E6] w-[326px] h-[131px] md:w-full">
+
+          <div className="">
+            <h3 className="font-roboto text-[18px] text-[#545454]">{"Total Earnings"}</h3>
+            <h3 className="font-roboto text-[32px] text-[#333333]">
               {`$8920 `}
             </h3>
           </div>
         </div>
 
-        <div className="flex items-center justify-center gap-6 px-[24px] border border-black  py-[20px] rounded-lg space-y-3 bg-white w-96 md:w-full">
-        
-          <div  className="text-center">
-            <h3 className="text-[20px]">{"Total User"}</h3>
-            <h3 className="text-[30px] font-extralight">1250</h3>
+        <div className=" flex items-center  gap-6  px-[24px] py-[20px] rounded-[16px] space-y-3 bg-[#DDE3E6] w-[326px] h-[131px] md:w-full">
+
+          <div className="">
+            <h3 className="font-roboto text-[18px] text-[#545454]">{"Total Users"}</h3>
+            <h3 className="font-roboto text-[32px] text-[#333333]">6500</h3>
           </div>
         </div>
 
-        <div className="flex items-center justify-center gap-6 border border-black px-[24px] py-[20px] rounded-lg space-y-3 bg-white w-96 md:w-full">
-          
-          <div  className="text-center">
-            <h3 className="text-[20px]">{"Total Driver"}</h3>
-            <h3 className="text-[30px] font-extralight">740</h3>
+        <div className=" flex items-center  gap-6  px-[24px] py-[20px] rounded-[16px] space-y-3 bg-[#DDE3E6] w-[326px] h-[131px] md:w-full">
+
+          <div className="">
+            <h3 className="font-roboto text-[18px] text-[#545454]">{"Total Doctors"}</h3>
+            <h3 className="font-roboto text-[32px] text-[#333333]">740</h3>
           </div>
         </div>
 
-        
+
       </div>
-      <BarChartComponent/>
+      <BarChartComponent />
       <DashboardHomeTable />
     </div>
   );
