89e2339eb8bb781bd180fad3bc961a66808e11a2
Raiyan109 <kraiyan109@gmail.com>
2025-02-10 09:40:44 +0600

    personal info

--- src/pages/Profile/MyProfile.jsx
+++ src/pages/Profile/MyProfile.jsx
@@ -8,6 +8,7 @@ import PageHeading from "../../Components/PageHeading";
 import PasswordChangeModalForm from "../../Components/User/PasswordChangeModalForm";
 import { FaAngleLeft } from "react-icons/fa6";
 import { FaRegEdit } from "react-icons/fa";
+import { GoArrowLeft } from "react-icons/go";
 
 
 const MyProfile = () => {
@@ -23,96 +24,97 @@ const MyProfile = () => {
   // console.log(code);
   return (
     <>
-      <div className="flex items-center gap-2 text-xl cursor-pointer" onClick={() => navigate(-1)}>
-        <FaAngleLeft />
-        <h1>Personal information</h1>
+      <div className="flex justify-between items-center">
+        {/* Back button */}
+        <div className="flex items-center gap-2 text-xl cursor-pointer" onClick={() => navigate(-1)}>
+          <GoArrowLeft size={30} />
+          <h1 className="font-roboto text-[24px]">Personal information</h1>
+        </div>
+
+        {/* Edit button */}
+        <div className="py-4 px-8 flex justify-end items-center">
+          {/* <h6 className="text-2xl text-white">Personal Information</h6> */}
+          <Button
+            onClick={(e) => navigate(`edit`)}
+            size="large"
+            type="default"
+            className="px-8 bg-[#90A4AE] text-white rounded-full font-semibold"
+          >
+            <FaRegEdit />
+            Edit Profile
+          </Button>
+        </div>
       </div>
-      <div className="rounded-lg py-4 border-lightGray border-2 shadow-lg mt-8 bg-white">
-        <h3 className="text-2xl text-black mb-4 pl-5 border-b-2 border-lightGray/40 pb-3">
-          Personal information
-        </h3>
-        <div>
-          <div className="space-y-[24px] min-h-[83vh] bg-light-gray rounded-2xl">
 
-            <div className="w-full">
-
-              <div className="py-4 px-8 flex justify-end items-center">
-                {/* <h6 className="text-2xl text-white">Personal Information</h6> */}
-                <Button
-                  onClick={(e) => navigate(`edit`)}
-                  size="large"
-                  type="default"
-                  className="px-8 bg-black text-white hover:bg-black/90 rounded-full font-semibold"
-                >
-                  <FaRegEdit />
-                  Edit Profile
-                </Button>
+      <div className="pt-[58px]">
+        <div className="space-y-[24px] min-h-[83vh] bg-light-gray rounded-2xl">
+          <Form
+            name="basic"
+            layout="vertical"
+            className="w-full flex gap-x-[24px]"
+            autoComplete="off"
+            initialValues={{
+              name: profileData.name,
+              email: profileData.email,
+            }}
+          >
+            {/* Profile Image */}
+            <div className="col-span-3 space-y-6 ">
+              <div className="min-h-[360px] w-[300px] rounded-xl flex flex-col items-center justify-center p-8 bg-[#DDE3E6]">
+                <div className="my-2">
+                  <img
+                    src={dashProfile}
+                    alt=""
+                    className="h-28 w-28 rounded-full border-4 border-black"
+                  />
+                </div>
+                <h5 className="text-lg text-[#222222]">{"Profile"}</h5>
+                <h4 className="text-2xl text-[#222222]">{"Admin"}</h4>
               </div>
 
-              <Form
-                name="basic"
-                layout="vertical"
-                className="w-full grid grid-cols-12 gap-x-10 px-14 py-8"
-                autoComplete="off"
-                initialValues={{
-                  name: profileData.name,
-                  email: profileData.email,
-                }}
-              >
-                <div className="col-span-3 space-y-6 ">
-                  <div className="min-h-[300px] flex flex-col items-center justify-center p-8 border border-black bg-lightGray/15">
-                    <div className="my-2">
-                      <img
-                        src={dashProfile}
-                        alt=""
-                        className="h-28 w-28 rounded-full border-4 border-black"
-                      />
-                    </div>
-                    <h5 className="text-lg text-[#222222]">{"Profile"}</h5>
-                    <h4 className="text-2xl text-[#222222]">{"Admin"}</h4>
-                  </div>
-
-                </div>
-                <div className="col-span-9 space-y-[14px] w-full">
-                  <Form.Item
-                    className="text-lg  font-medium text-black -mb-1"
-                    label="Name"
-                    name="name"
-                  >
-                    <Input
-                      readOnly
-                      size="large"
-                      className="h-[53px] rounded-lg"
-                    />
-                  </Form.Item>
-                  <Form.Item
-                    className="text-lg  font-medium text-black"
-                    label="Email"
-                    name="email"
-                  >
-                    <Input
-                      readOnly
-                      size="large"
-                      className="h-[53px] rounded-lg"
-                    />
-                  </Form.Item>
-
-                  <Form.Item
-                    className="text-lg text-[#222222] font-medium"
-                    label="Phone Number"
-                    name="phone"
-                  >
-                    <PhoneCountryInput />
-                  </Form.Item>
-                </div>
-              </Form>
             </div>
-            <PasswordChangeModalForm
-              isModalOpen={isModalOpen}
-              setIsModalOpen={setIsModalOpen}
 
-            />
-          </div>
+            {/* Profile Inputs */}
+            <div className="col-span-9 space-y-[14px] w-full">
+              <Form.Item
+                className="font-roboto text-[18px]"
+                label={<span className="font-roboto text-[14px] text-black/90">Name</span>}
+                name="name"
+              >
+                <Input
+                  readOnly
+                  size="large"
+                  className="h-[53px] rounded-lg"
+                  style={{ border: '1px solid #DDDEE0' }}
+                />
+              </Form.Item>
+              <Form.Item
+                className="font-roboto text-[18px]"
+                label={<span className="font-roboto text-[14px] text-black/90">Email</span>}
+                name="email"
+              >
+                <Input
+                  readOnly
+                  size="large"
+                  className="h-[53px] rounded-lg"
+                  style={{ border: '1px solid #DDDEE0' }}
+                />
+              </Form.Item>
+
+              <Form.Item
+                className="font-roboto text-[18px]"
+                label={<span className="font-roboto text-[14px] text-black/90">Phone Number</span>}
+                name="phone"
+              >
+                <PhoneCountryInput />
+              </Form.Item>
+            </div>
+          </Form>
+
+          <PasswordChangeModalForm
+            isModalOpen={isModalOpen}
+            setIsModalOpen={setIsModalOpen}
+          />
         </div>
         <div className="p-[24px] pt-0.5">
           <Outlet />
